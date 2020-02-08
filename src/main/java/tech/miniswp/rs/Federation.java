1687674073c95f51a8afa9d942d029c4b9a26203
Rafael Espino <rafael.espino@gmail.com>
2020-02-08 18:46:41 +0100

    Updated fed endpoints

--- src/main/java/tech/miniswp/rs/Federation.java
+++ src/main/java/tech/miniswp/rs/Federation.java
@@ -11,12 +11,14 @@ public class Federation {
 
     public static void main(String[] args)  {
         Repository repository = FedXFactory.newFederation()
-                .withSparqlEndpoint("http://dbpedia.org/sparql")
-                .withSparqlEndpoint("https://query.wikidata.org/sparql")
+                .withSparqlEndpoint("http://admin:admin@localhost:5820/bank/query")
+                //.withSparqlEndpoint("https://query.wikidata.org/sparql")
+                .withSparqlEndpoint("http://localhost:7200/repositories/bank")
                 .create();
 
         try (RepositoryConnection conn = repository.getConnection()) {
 
+            /*
             String query =
                     "PREFIX wd: <http://www.wikidata.org/entity/> "
                             + "PREFIX wdt: <http://www.wikidata.org/prop/direct/> "
@@ -25,6 +27,18 @@ public class Federation {
                             + " ?country <http://www.w3.org/2002/07/owl#sameAs> ?countrySameAs . "
                             + " ?countrySameAs wdt:P2131 ?gdp ."
                             + "}";
+             */
+
+            String query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                    "PREFIX bank: <http://miniswp.tech/demo/bank#>\n" +
+                    "\n" +
+                    "SELECT ?o2 ?o1\n" +
+                    "WHERE {\n" +
+                    "    ?s bank:phone \"01010101\" .\n" +
+                    "    ?s bank:appliedForLoan ?o .\n" +
+                    "    ?o bank:fromIP ?o1 .\n" +
+                    "    ?o2 bank:fromIP ?o1\n" +
+                    "} \n";
 
             TupleQuery tq = conn.prepareTupleQuery(query);
             try (TupleQueryResult tqRes = tq.evaluate()) {
